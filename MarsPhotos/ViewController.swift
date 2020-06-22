7d1fe2ce759e4df2fa140ed3b1e42a52fdc6d7f1
Gh0st <oti@ameeca.io>
2020-06-22 12:57:48 -0500

    Initial Commit

---
+++ MarsPhotos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarsPhotos
+//
+//  Created by Oti Oritsejafor on 6/22/20.
+//  Copyright © 2020 Magloboid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
